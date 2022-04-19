2a5afbf2a93a48fb3b42f88d0995f07546389870
Aminu Ismail <63140904+ibnabbas1429@users.noreply.github.com>
2022-04-19 22:46:29 +0100

    A simple dictionary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+'''Dictionary is a data structure in python that holds key:value pair. Key-value is provided in the dictionary 
+to make it more optimized. '''
+
+# A simple dictionary in pyhon.
+python_Book = { "author" : "Eric Matthes", "Title":"Python Crash Course","Edition":"2nd Edition"}
+print(python_Book)
+
+# Using the dictionary with f-string
+print(f"The author of {python_Book['Title']} is {python_Book['author']}")
+
